cea5d22fad1fe4d57a4d18d9edb51d794cf560ce
Pranav Balaji <73339142+greenfish8090@users.noreply.github.com>
2021-04-14 23:15:58 +0530

    Added react build

--- ReactApp/build/dodoshows/__init__.py
+++ ReactApp/build/dodoshows/__init__.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
-from flask_mail import Mail
 from dotenv import load_dotenv, find_dotenv
 import os
 
 mysql = MySQL()
 jwt = JWTManager()
 cors = CORS()
-mail = Mail()
-
 
 from dodoshows.search import search_blueprint
 from dodoshows.movies import movies_blueprint
@@ -22,7 +19,7 @@ from dodoshows.seats import seats_blueprint
 
 
 def create_app():
-    app = Flask(__name__,static_folder='../ReactApp/build',static_url_path='')
+    app = Flask(__name__)
 
     load_dotenv("/.env")
     app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
@@ -32,20 +29,11 @@ def create_app():
     app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
     app.config["MYSQL_CURSORCLASS"] = "DictCursor"
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_KEY")
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 24 * 60 * 60
     app.config["CORS_HEADERS"] = "Content-Type"
-    app.config["DEBUG"] = True
-    app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-    app.config["MAIL_PORT"] = 465
-    app.config["MAIL_USE_TLS"] = False
-    app.config["MAIL_USE_SSL"] = True
-    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
     mysql.init_app(app)
     jwt.init_app(app)
     cors.init_app(app)
-    mail.init_app(app)
 
     app.register_blueprint(search_blueprint)
     app.register_blueprint(movies_blueprint)
@@ -54,10 +42,6 @@ def create_app():
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(shows_blueprint)
     app.register_blueprint(seats_blueprint)
-
-    @app.route('/')
-    def serve():
-        return send_from_directory(app.static_folder, 'index.html')
 
     return app
 
---
+++ ReactApp/build/dodoshows/users.py
@@ -0,0 +1,149 @@
+from flask import jsonify, request, Blueprint
+from flask_jwt_extended import (
+    get_jwt_identity,
+    jwt_required,
+    jwt_optional,
+    create_access_token,
+)
+from dodoshows import mysql
+
+users_blueprint = Blueprint("users", __name__, url_prefix="/users")
+
+
+@users_blueprint.route("/")
+def getAllUsers():
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """SELECT user_id, username, city_name
+            FROM user
+            INNER JOIN city ON user.city_id = city.city_id
+            LIMIT 50"""
+    )
+    results = cur.fetchall()
+    cur.close()
+    return jsonify(results)
+
+
+@users_blueprint.route("/<user_id>")
+def getUser(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """SELECT user_id, username, city.city_name
+            FROM user
+            INNER JOIN city ON user.city_id = city.city_id
+            WHERE user_id = %s""",
+        [user_id],
+    )
+    result = cur.fetchone()
+    cur.close()
+    return jsonify(result)
+
+
+@users_blueprint.route("/<user_id>/lists")
+@jwt_optional
+def getUserLists(user_id):
+    cur = mysql.connection.cursor()
+    if user_id == get_jwt_identity():
+        cur.execute(
+            """SELECT entry.list_id, user_list.list_name, user_list.is_private, movie.movie_id, movie.movie_title, rating.watch_status, rating.score, rating.review
+                FROM entry
+                INNER JOIN user_list ON entry.list_id = user_list.list_id
+                INNER JOIN movie ON entry.movie_id = movie.movie_id
+                INNER JOIN rating ON user_list.user_id = rating.user_id
+                                    AND entry.movie_id = rating.movie_id
+                WHERE user_list.user_id = %s""",
+            [user_id],
+        )
+    else:
+        cur.execute(
+            """SELECT entry.list_id, user_list.list_name, user_list.is_private, movie.movie_id, movie.movie_title, rating.watch_status, rating.score, rating.review
+                FROM entry
+                INNER JOIN user_list ON entry.list_id = user_list.list_id
+                INNER JOIN movie ON entry.movie_id = movie.movie_id
+                INNER JOIN rating ON user_list.user_id = rating.user_id
+                                    AND entry.movie_id = rating.movie_id
+                WHERE user_list.user_id = %s AND user_list.is_private = 0""",
+            [user_id],
+        )
+    results = cur.fetchall()
+    cur.close()
+    return jsonify(results)
+
+
+@users_blueprint.route("/<user_id>", methods=["POST"])
+@jwt_required
+def addRating(user_id):
+    if str(user_id) != str(get_jwt_identity()):
+        return {"error": "not authorized"}
+
+    movie_id = request.json["movie_id"]
+    watch_status = request.json["watch_status"]
+    score = request.json["score"]
+    review = request.json["review"]
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """INSERT INTO rating
+            VALUES (DEFAULT, %s, %s, %s, %s, %s)""",
+        [user_id, movie_id, watch_status, score, review],
+    )
+    mysql.connection.commit()
+    cur.close()
+    return None
+
+
+@users_blueprint.route("/<user_id>/<movie_id>", methods=["DELETE"])
+@jwt_required
+def deleteRating(user_id, movie_id):
+    if str(user_id) != str(get_jwt_identity()):
+        return {"error": "not authorized"}
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """DELETE FROM rating
+            WHERE user_id = %s AND movie_id = %s""",
+        [user_id, movie_id],
+    )
+    mysql.connection.commit()
+    cur.close()
+    return None
+
+
+@users_blueprint.route("/<user_id>/<movie_id>", methods=["PUT"])
+@jwt_required
+def updateRating(user_id, movie_id):
+    if str(user_id) != str(get_jwt_identity()):
+        return {"error": "not authorized"}
+
+    watch_status = request.json["watch_status"]
+    score = request.json["score"]
+    review = request.json["review"]
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """UPDATE rating
+            SET watch_status = %s, score = %s, review = %s
+            WHERE user_id = %s AND movie_id = %s""",
+        [watch_status, score, review, user_id, movie_id],
+    )
+    mysql.connection.commit()
+    cur.close()
+    return None
+
+
+@users_blueprint.route("/<user_id>/<movie_id>/watched", methods=["PUT"])
+@jwt_required
+def setWatched(user_id, movie_id):
+    if str(user_id) != str(get_jwt_identity()):
+        return {"error": "not authorized"}
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """UPDATE rating
+            SET watch_status = 1
+            WHERE user_id = %s AND movie_id = %s""",
+        [user_id, movie_id],
+    )
+    mysql.connection.commit()
+    cur.close()
+    return None
